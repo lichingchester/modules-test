dba07c09a1ad5e4dc605b5893f8e6c1a5fc9cd96
lichingchester <lichingchester@gmail.com>
2018-12-13 19:26:34 +0800

    init npm run build function

--- bin/command.js
+++ bin/command.js
@@ -1,13 +1,8 @@
 'use strict';
 
-const path = require('path');
-// const log = require('./logger');
 const temp = require('./commands/index');
 
 const command = (input = []) => {
-  // log.debugStatus('command start'); 
-  // log.debug(`input: ${input}`);
-
   const command = input.length > 0 ? input[0] : null;
   switch(command){
     case 'init':
--- bin/commands/build.js
+++ bin/commands/build.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const logger = require('../logger');
+
+const build = () => {
+    logger.info('Building site...');
+
+    
+
+}
+
+module.exports = build;
